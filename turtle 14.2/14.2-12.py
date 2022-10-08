20a4da16d675d399e79c54aa7ed66ef1a5917bd4
igor <igor.pravdin163@gmail.com>
2022-10-08 21:51:28 +0400

    12

---
+++ turtle 14.2/14.2-12.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.speed(7)
+
+t.penup()
+t.goto(0, 100)
+t.pendown()
+t.dot()
+for i in range(-120, 120, 20):
+  t.pencolor("DeepSkyBlue")
+  t.goto(i, -70)
+  t.pencolor("blue")
+  t.pensize(3)
+  t.dot()
+  t.pensize(1)
+  t.pencolor("DeepSkyBlue")
+  t.goto(0, 100)
