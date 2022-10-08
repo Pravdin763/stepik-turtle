2c1d440afd6dc6a7331147bb7ec9c183badb2bcc
igor <igor.pravdin163@gmail.com>
2022-10-08 21:53:27 +0400

    12 + коммент.

--- turtle 14.2/14.2-12.py
+++ turtle 14.2/14.2-12.py
@@ -1,6 +1,7 @@
 # Напишите программу, которая рисует изображение в соответствии с образцом (ёлочку)
 
 import turtle as t
+
 t.speed(7)
 
 t.penup()
@@ -8,11 +9,11 @@ t.goto(0, 100)
 t.pendown()
 t.dot()
 for i in range(-120, 120, 20):
-  t.pencolor("DeepSkyBlue")
-  t.goto(i, -70)
-  t.pencolor("blue")
-  t.pensize(3)
-  t.dot()
-  t.pensize(1)
-  t.pencolor("DeepSkyBlue")
-  t.goto(0, 100)
+    t.pencolor("DeepSkyBlue")
+    t.goto(i, -70)
+    t.pencolor("blue")
+    t.pensize(3)
+    t.dot()
+    t.pensize(1)
+    t.pencolor("DeepSkyBlue")
+    t.goto(0, 100)
