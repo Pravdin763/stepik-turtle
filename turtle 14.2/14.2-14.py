41eddd3999296db41ad49af3463abfddad592cfe
igor <igor.pravdin163@gmail.com>
2022-10-08 22:39:09 +0400

    мишка не доделан

---
+++ turtle 14.2/14.2-14.py
@@ -0,0 +1,39 @@
+# Напишите программу, которая рисует изображение мишки в соответствии с образцом.
+# https://stepik.org/lesson/330014/step/14?unit=313365
+
+import turtle as t
+
+#t.speed(8)
+
+
+t.goto(0, -50)
+t.penup()
+t.goto(0, -70)
+t.pendown()
+t.circle(100)
+t.circle(50)
+
+t.penup()
+t.goto(0, -2)
+t.pendown()
+t.circle(10)
+
+t.penup()
+t.goto(40, 25)
+t.pendown()
+t.circle(13)
+
+t.penup()
+t.goto(-40, 25)
+t.pendown()
+t.circle(13)
+
+t.penup()
+t.goto(85, 100)
+t.pendown()
+t.circle(35)
+
+t.penup()
+t.goto(-85, 100)
+t.pendown()
+t.circle(35)
