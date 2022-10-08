fb3cdecc9962805cd5d57dc2837aeea28261c44d
igor <igor.pravdin163@gmail.com>
2022-10-08 22:53:57 +0400

    мишка готов

--- turtle 14.2/14.2-14.py
+++ turtle 14.2/14.2-14.py
@@ -6,36 +6,40 @@ import turtle as t
 t.speed(10)
 
 
-t.goto(0, -50)
-t.penup()
-t.goto(0, -70)
-t.pendown()
-t.circle(100)
-t.circle(50)
+def head():
+    t.goto(0, -50)
+    t.penup()
+    t.goto(0, -70)
+    t.pendown()
+    t.circle(100)
+    t.circle(50)
 
 
+def moska():
+    t.penup()
+    t.goto(0, -2)
+    t.pendown()
+    t.circle(10)
 
-t.penup()
-t.goto(0, -2)
-t.pendown()
-t.circle(10)
 
-t.penup()
-t.goto(45, 33)
-t.pensize(12)
-t.dot()
+def eyes():
+    t.pensize(12)
+    for i in range(-45, 46, 90):
+        t.penup()
+        t.goto(i, 33)
+        t.dot()
+    t.pensize(1)
 
-t.penup()
-t.goto(-45, 33)
-t.dot()
-t.pensize(1)
 
-t.penup()
-t.goto(85, 100)
-t.pendown()
-t.circle(35)
+def ears():
+    for i in range(-85, 86, 170):
+        t.penup()
+        t.goto(i, 100)
+        t.pendown()
+        t.circle(35)
 
-t.penup()
-t.goto(-85, 100)
-t.pendown()
-t.circle(35)
+
+head()
+moska()
+eyes()
+ears()
