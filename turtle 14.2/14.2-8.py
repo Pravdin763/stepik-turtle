707e8e63c0a8cdc352129b14ca8ec860e2a8c6c0
igor <igor.pravdin163@gmail.com>
2022-10-08 20:42:28 +0400

    8

---
+++ turtle 14.2/14.2-8.py
@@ -0,0 +1,15 @@
+import turtle as t
+import random
+
+color = ['red', 'blue', 'gold', 'green', 'LightSeaGreen']
+
+x = 5
+size = 1
+
+for i in range(50):
+    t.pencolor(random.choice(color))
+    t.pensize(size)
+    t.forward(x)
+    t.left(45)
+    x += 3
+    size += 0.5
